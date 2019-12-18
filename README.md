d5e176dc2392bb220196d2def5e4e65cd06f7a07
Ashley Patricks <ashley@info.nl>
2019-12-18 17:38:39 +0100

    Deploy to Github repo

---
+++ README.md
@@ -0,0 +1,28 @@
+# Full Stack React & Express Contacts Manager
+
+This app is comprised of an Express server with authentication and CRUD capabilities. You can manage your contacts after registering and logging in to the app.
+
+The React App uses the Context API and React Hooks.
+
+The backend uses MongoDB & Mongoose, so either hook up your local MongoDB Server or just use Mongo Cloud Atlas.
+
+## Installation Instructions
+
+1. Install the client app:
+
+> > npm run clientinstall
+
+2. Install the server app:
+
+> > npm install
+
+3. Run both of the apps side by side:
+
+> > npm run dev
+
+### Bugs
+
+I will document this app properly at some point - but any questions or bugs please hit me up on Github.
+
+Cheers,
+Ashley
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const expressValidator = require('express-validator');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -16,5 +17,16 @@ app.use(expressValidator());
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
+
+// Serve static assets in Production (Serve React)
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder (The static assets that React builds for us)
+  app.use(express.static('client/build'));
+
+  // This route is for anything that does not include the above api routes. That's why we put this below those routes.
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
